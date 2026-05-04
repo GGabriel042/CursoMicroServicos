0683189067b22e14bac9719e511a13f7acd0136c
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-05-04 16:50:25 -0300

    Get All, Get By Id, Create

--- src/main/java/br/com/cursoMicroServicos/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/cursoMicroServicos/pagamentos/service/PagamentoService.java
@@ -1,9 +1,17 @@
 package br.com.cursoMicroServicos.pagamentos.service;
 
+import br.com.cursoMicroServicos.pagamentos.dto.PagamentoDto;
+import br.com.cursoMicroServicos.pagamentos.model.Pagamento;
+import br.com.cursoMicroServicos.pagamentos.model.Status;
 import br.com.cursoMicroServicos.pagamentos.repository.PagamentosRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +20,26 @@ public class PagamentoService {
     private final PagamentosRepository repository;
 
     private final ModelMapper modelMapper;
+
+    public List<PagamentoDto> getAll(){
+        return repository
+                .findAll()
+                .stream()
+                .map(p -> modelMapper.map(p, PagamentoDto.class))
+                .collect(Collectors.toList());
+    }
+
+    public PagamentoDto getById(Long id) {
+        Optional<Pagamento> optionalPagamento = repository.findById(id);
+        Pagamento pagamento = optionalPagamento.orElseThrow(EntityNotFoundException::new);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto createPagamento(PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
 }
