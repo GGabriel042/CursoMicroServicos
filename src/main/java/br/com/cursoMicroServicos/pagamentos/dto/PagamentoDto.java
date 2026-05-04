1667cf5ae38c89603fe1cc8722a06516ad2d623a
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-05-04 16:25:09 -0300

    Pagamento DTO

---
+++ src/main/java/br/com/cursoMicroServicos/pagamentos/dto/PagamentoDto.java
@@ -0,0 +1,29 @@
+package br.com.cursoMicroServicos.pagamentos.dto;
+
+import br.com.cursoMicroServicos.pagamentos.model.Status;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PagamentoDto {
+
+    private Long id;
+
+    private BigDecimal valor;
+
+    private String nome;
+
+    private String numero;
+
+    private String expiracao;
+
+    private String codigo;
+
+    private Status status;
+
+    private Long pedidoId;
+
+    private Long formaDePagamentoId;
+
+}
